a2ff3d12219178ded7e6456b2409c1d283fae327
Michael Zipp <milonti1@gmail.com>
2018-04-30 16:16:10 -0400

    Add a click handler

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import TimeSlot from './components/cmp_TimeSlot.jsx'
-import DaySched from './components/cmp_DaySched.jsx'
+// import DaySched from './components/cmp_DaySched.jsx'
 import WeekSched from './components/cmp_WeekSched.jsx'
 
 class App extends Component {
@@ -16,41 +16,6 @@ class App extends Component {
         <p className="App-intro">
           Look it's ELECTRON
         </p>
-        { /** <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Sunday" dayType="weekend" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Monday" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Tuesday" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Wednesday" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Thursday" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Friday" />
-          </tbody>
-        </table>
-        <table className="weekCalendar">
-          <tbody>
-            <DaySched dayOfWeek="Saturday" dayType="weekend" />
-          </tbody>
-        </table> **/ }
         <WeekSched id='week1' />
       </div>
     );
--- src/components/cmp_TimeSlot.jsx
+++ src/components/cmp_TimeSlot.jsx
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
 var uid = require('react-html-id');
+
 class TimeSlot extends Component {
   constructor(props){
     super(props);
     uid.enableUniqueIds(this);
+    this.fillState = {
+      UNFILLED  :'unfilled',
+      FILLING   :'filling',
+      FILLED    :'filled'
+    }
     this.state = {
-      filled: false,
-      person: "",
-      phone: ""
+      filled: this.fillState.UNFILLED,
+      person: "testPerson",
+      phone: "(123) 456-7890",
+      type: "Phone Call"
     }
     this.id = this.getUniqueId('timeslot');
+    this.handleClick = this.handleClick.bind(this);
   }
   render() {
-    if(this.state.filled){
-      return this.renderFilled();
+    if(this.state.filled !== this.fillState.FILLED ){
+      return this.renderUnfilled();
     }
-    else return this.renderUnfilled();
+    else return this.renderFilled();
   }
   renderUnfilled() {
     return (
-      <div className="timeSlot unfilled" id={this.id}>
+      <div className="timeSlot unfilled" id={this.id} onClick={this.handleClick}>
         <span className="time">{this.props.time}</span>
       </div>
     )
   }
   renderFilled() {
-      <div className="timeSlot filled" id={this.id}>
+    return (
+      <div className="timeSlot filled" id={this.id} onClick={this.handleClick} >
         <span className="time">{this.props.time}</span>
         <span className="meetingInfo">Name: {this.state.person}</span>
         <span className="meetingInfo">Phone #: {this.state.phone}</span>
         <span className="personName">Event Type: {this.state.type}</span>
       </div>
+    )
+  }
+  handleClick(e){
+    this.setState(prevState => ({
+      filled: (prevState.filled === this.fillState.UNFILLED ? this.fillState.FILLED : this.fillState.UNFILLED),
+    }))
   }
 }
 TimeSlot.defaultProps = {
